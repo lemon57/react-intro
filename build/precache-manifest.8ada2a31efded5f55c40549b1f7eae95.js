ea26b633ce12bcd847436daa49636a73aa7d2072
Roman Lemon <gzeek57@gmail.com>
2019-02-06 15:47:54 +0100

    after soutce

---
+++ build/precache-manifest.8ada2a31efded5f55c40549b1f7eae95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bef8bd6fe6c496820fb4",
+    "url": "/react-intro/static/js/runtime~main.bef8bd6f.js"
+  },
+  {
+    "revision": "22f0e94133c4f9f0d074",
+    "url": "/react-intro/static/js/main.22f0e941.chunk.js"
+  },
+  {
+    "revision": "68a269214e7df4bda03b",
+    "url": "/react-intro/static/js/1.68a26921.chunk.js"
+  },
+  {
+    "revision": "22f0e94133c4f9f0d074",
+    "url": "/react-intro/static/css/main.cc8e80df.chunk.css"
+  },
+  {
+    "revision": "49619c144d6add6bbfb5139344a6da3f",
+    "url": "/react-intro/index.html"
+  }
+];
